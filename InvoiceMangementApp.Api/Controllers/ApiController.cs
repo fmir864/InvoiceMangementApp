2bcc74bf7b791c6b4068c6630638536ed498746f
FM Irshad <irshad@datamation.lk>
2020-08-31 17:59:48 +0530

    Invoice controller

---
+++ InvoiceMangementApp.Api/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceMangementApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ InvoiceMangementApp.Api/Controllers/InvoicesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvoiceManagementApp.Application.Invoices.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceMangementApp.Api.Controllers
+{
+    [Authorize]
+    public class InvoicesController : ApiController
+    {
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
