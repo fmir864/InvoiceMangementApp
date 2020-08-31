37b1c1f962afc4b4b87b648e43ffa8cba6375b99
FM Irshad <irshad@datamation.lk>
2020-08-31 16:39:47 +0530

    Add Invoice related domain classes

---
+++ InvoiceManagementApp.Domain/Common/AuditEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.Domain.Common
+{
+    public class AuditEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+    }
+}
---
+++ InvoiceManagementApp.Domain/Entities/InvoiceItem.cs
@@ -0,0 +1,19 @@
+﻿using InvoiceManagementApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.Domain.Entities
+{
+    public class InvoiceItem : AuditEntity
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public string Item { get; set; }
+        public double Quantity { get; set; }
+        public double Rate { get; set; }
+        public Invoice Invoice { get; set; }
+    }
+}
