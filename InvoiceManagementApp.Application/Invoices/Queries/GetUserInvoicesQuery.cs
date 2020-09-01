a2f419f87e0a072fff398218b4716bc1939722c0
FM Irshad <irshad@datamation.lk>
2020-09-01 10:21:55 +0530

    Query for Invoices

---
+++ InvoiceManagementApp.Application/Invoices/Queries/GetUserInvoicesQuery.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceManagementApp.Application.Invoices.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.Application.Invoices.Queries
+{
+    public class GetUserInvoicesQuery : IRequest<IList<InvoiceVm>>
+    {
+        public string User { get; set; }
+    }
+}
--- InvoiceMangementApp.Api/Controllers/InvoicesController.cs
+++ InvoiceMangementApp.Api/Controllers/InvoicesController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InvoiceManagementApp.Application.Common.Interfaces;
 using InvoiceManagementApp.Application.Invoices.Commands;
+using InvoiceManagementApp.Application.Invoices.Queries;
+using InvoiceManagementApp.Application.Invoices.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +16,23 @@ namespace InvoiceMangementApp.Api.Controllers
     public class InvoicesController : ApiController
     {
 
+        private readonly ICurrentUserService _currentUserService;
+
+        public InvoicesController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
         [HttpPost]
         public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<IList<InvoiceVm>> Get()
+        {
+            return await Mediator.Send(new GetUserInvoicesQuery { User = _currentUserService.UserId });
         }
     }
 }
